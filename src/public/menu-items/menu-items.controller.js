3b0cedbef5fe4b4ac5b945711e9734762726fb1a
uonlyyoloonce <rookin83@gmail.com>
2017-07-28 16:33:59 -0400

    assgin5

---
+++ src/public/menu-items/menu-items.controller.js
@@ -0,0 +1,13 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('MenuItemsController', MenuItemsController);
+
+MenuItemsController.$inject = [];
+function MenuItemsController() {
+  var $ctrl = this;
+ 
+}
+
+})();
---
+++ src/public/menu-signup/menu-signup.controller.js
@@ -0,0 +1,13 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('MenuSignupController', MenuSignupController);
+
+//MenuSignupController.$inject = ['menuItems'];
+function MenuSignupController() {
+  var reg = this;
+
+}
+
+})();
